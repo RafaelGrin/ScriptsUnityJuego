ab351adc16c293d482fc6f9b2fe2a389d9ccf1d1
RafaelGrin <47076982@escuelabialik.edu.ar>
2023-11-09 14:07:57 -0300

    arreglocartel

--- Agarrar-llave.cs
+++ Agarrar-llave.cs
@@ -1,41 +1,48 @@
 using UnityEngine;
+using UnityEngine.UI; // Importa el espacio de nombres para UI
 
 public class Player : MonoBehaviour
 {
-    // Declaración de un evento para notificar cuando se recoge una llave
     public delegate void LlaveRecogidaEventHandler(string nombreLlave);
     public static event LlaveRecogidaEventHandler OnLlaveRecogida;
 
+    public Text mensajeText; // Asigna el objeto de texto en el Inspector
+
+    private void Start()
+    {
+        // Asegúrate de que el mensaje de texto esté desactivado al principio
+        mensajeText.gameObject.SetActive(false);
+    }
+
     private void Update()
     {
-        // Detecta si el jugador presiona la tecla "E"
         if (Input.GetKeyDown(KeyCode.E))
         {
-            // Realiza un raycast hacia adelante desde la posición del jugador
             Ray ray = new Ray(transform.position, transform.forward);
             RaycastHit hit;
 
-            // Determina si el rayo colisiona con un objeto
             if (Physics.Raycast(ray, out hit, 2.0f))
             {
-                // Verifica si el objeto tiene una etiqueta de "Llave" y recoge la llave
                 if (hit.collider.CompareTag("Llave"))
                 {
-                    // Desactiva el objeto de la llave
                     hit.collider.gameObject.SetActive(false);
-
-                    // Notifica que se ha recogido la llave con su nombre
                     OnLlaveRecogida?.Invoke(hit.collider.gameObject.name);
 
-                  if (coinGrabSound != null)
-                    {
-                        audioSource.PlayOneShot(coinGrabSound);
-                    }
+                    // Muestra el mensaje en el objeto de texto
+                    mensajeText.text = "Muy bien, has agarrado una llave";
+                    mensajeText.gameObject.SetActive(true);
 
-
-
+                    // Luego, puedes ocultar el mensaje después de un tiempo si lo deseas
+                    StartCoroutine(OcultarMensaje(3.0f)); // Oculta el mensaje después de 3 segundos
                 }
             }
         }
+    }
+
+    // Método para ocultar el mensaje después de un tiempo
+    private IEnumerator OcultarMensaje(float tiempo)
+    {
+        yield return new WaitForSeconds(tiempo);
+        mensajeText.gameObject.SetActive(false);
     }
 }
